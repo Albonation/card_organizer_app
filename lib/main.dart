9aa50b7d8476dd2f87eee2aa94d3f28c5376dffb
Alan Pruett <cpruett3@student.gsu.edu>
2026-03-05 17:16:26 -0500

    chore: add dependencies for sqflite_common_ffi and windows support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:card_organizer_app/screens/folders_screen.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
+import 'screens/folders_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(const MyApp());
 }
 
